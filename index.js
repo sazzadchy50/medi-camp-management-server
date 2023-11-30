fc6812ab24da9b87bfc76bd66a6cfb33d2081bed
sazzadchy50 <sazzadchy50@gmail.com>
2023-11-30 23:44:15 +0600

    user get code update

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const campCollection = client
       .db("medi-Camp-management")
       .collection("allCamp");
